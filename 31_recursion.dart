46e7dc058e43411d5002b29433ea0e66adc033be
akhil <akhilsodvadiya7@gmail.com>
2023-09-18 18:08:19 +0530

    18-09-2023

--- 31_recursion.dart
+++ 31_recursion.dart
@@ -1,5 +1,12 @@
 void main()
 {
-  print("recursion");
-  print("recursion concept");
+  // future datatype
+  // future.delayed - return data in future
+  // timer.periodic - recursion
+  // Timer - not return anything
+  // Transform.rotate
+  // Transform.skew
+  // Transform.scale
+  // Progress bar - circular,liniear
+  
 }
