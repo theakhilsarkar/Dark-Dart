cf3363698f0acb28c36bff8a7f9ad49163e4e4a8
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2023-10-30 21:54:07 +0530

    Add files via upload

---
+++ exam/dart_random.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'c_questions.dart';
+import 'flutter_3pm.dart';
+
+void main() {
+  Random random = Random();
+  int name = random.nextInt(12);
+  int question1 = random.nextInt(39);
+  int question2 = random.nextInt(39);
+  int question3 = random.nextInt(39);
+  int question4 = random.nextInt(39);
+  int question5 = random.nextInt(39);
+
+  print("\n");
+  print("Name : ${studentListOf3pm[name]}");
+  print("");
+
+
+  print("Q1 : ${c_language_questionList[question1]}");
+  print("Q2 : ${c_language_questionList[question2]}");
+  print("Q3 : ${c_language_questionList[question3]}");
+  print("Q4 : ${c_language_questionList[question4]}");
+  print("Q5 : ${c_language_questionList[question5]}");
+
+  print("\n");
+}
