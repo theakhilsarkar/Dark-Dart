4cd9dc17f6feba0d64a8945cc4a0b2ffb04beb98
akhil <akhilsodvadiya7@gmail.com>
2024-01-06 15:32:03 +0530

    06-01-2023

--- Dart PQ/left_rotate.dart
+++ Dart PQ/left_rotate.dart
@@ -3,12 +3,17 @@ import 'dart:io';
 void main() {
   List l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  // if user enter : 2
+  // [1,2,3,4,5,6,7,8,9]
+  // [2,3,4,5,6,7,8,9,1]
+  // [3,4,5,6,7,8,9,1,2]
+
   stdout.write("Enter index to left rotate : ");
   int n = int.parse(stdin.readLineSync()!);
 
   print(l1);
-  for (int i = 0; i <= n; i++) {
-    int last = l1[i];
+  for (int i = 0; i < n; i++) {
+    int last = l1[0]; //1
     for (int j = 0; j < l1.length - 1; j++) {
       l1[j] = l1[j + 1];
     }
---
+++ Dart PQ/right_rotate.dart
@@ -0,0 +1,13 @@
+void main() {
+  List l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // List l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //ans = 8,9,1,2,3,4,5,6,7
+  print(l1);
+
+  int first = l1[l1.length - 1];
+  for (int i = 2; i < l1.length - 1; i++) {
+    
+  }
+
+  print(l1);
+}
