54bcf152309d2957bd0e9cd65896ad9941befcbc
akhil <akhilsodvadiya7@gmail.com>
2024-01-09 10:22:21 +0530

    09-01-2023

--- Dart PQ/right_rotate.dart
+++ Dart PQ/right_rotate.dart
@@ -1,12 +1,13 @@
 void main() {
-  List l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // List l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  //ans = 8,9,1,2,3,4,5,6,7
+  List l1 = [1, 2, 3, 4, 5];
   print(l1);
 
-  int first = l1[l1.length - 1];
-  for (int i = 2; i < l1.length - 1; i++) {
-    
+  for (int n = 1; n <= 3; n++) {
+    int temp = l1[l1.length - 1];
+    for (int i = l1.length - 2; i >= 0; i--) {
+      l1[i + 1] = l1[i];
+    }
+    l1[0] = temp;
   }
 
   print(l1);
