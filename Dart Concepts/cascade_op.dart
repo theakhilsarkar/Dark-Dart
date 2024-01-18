9c8862acd5cdb604d0469423cb1d478bd5f14642
akhil <akhilsodvadiya7@gmail.com>
2024-01-18 10:03:47 +0530

    18-01-2024

---
+++ Dart Concepts/cascade_op.dart
@@ -0,0 +1,34 @@
+import '../Dart Basics/21_set(4)2_two_class.dart';
+
+void main() {
+  Demo d1 = Demo();
+  d1.hello(i: 1);
+  d1.welcome();
+  d1.how();
+
+  d1
+    ..hello(i: 10)
+    ..how()
+    ..welcome()
+    ..a = 10
+    ..printf();
+}
+
+class Demo {
+  late int a, b;
+  void hello({required int i}) {
+    print("Hello Dart $i");
+  }
+
+  void welcome() {
+    print("welcome dart");
+  }
+
+  void how() {
+    print("how are you !");
+  }
+
+  void printf() {
+    print(a + b);
+  }
+}
