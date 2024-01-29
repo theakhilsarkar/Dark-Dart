7fe224bb4f4a2e5a62ce8e3b9ca7c55b1068ace3
akhil <akhilsodvadiya7@gmail.com>
2024-01-29 13:56:55 +0530

    29-01-2024

--- Exception/main.dart
+++ Exception/main.dart
@@ -1,31 +1,32 @@
 import 'dart:io';
 
 void main() {
-  // int x, y;
+  int x, y;
   // String z = "10";
 
-  // stdout.write("Enter x : ");
-  // x = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter x : ");
+
   // stdout.write("Enter y : ");
 
-  // try {
-  //   y = int.parse(stdin.readLineSync()!);
-  //   print(x / y);
-  // } on FormatException catch (e) {
-  //   print("formate exception on error $e");
-  // } catch (e) {
-  //   print("invalid input");
-  // } finally {
-  //   print("thank you");
-  // }
-
-  List l1 = [1, 2, 3, 4, 5];
-
   try {
-    print(l1[6]);
-  } on RangeError catch (e) {
-    print("Enter sahi input ! $e");
+    x = int.parse(stdin.readLineSync()!);
+    y = int.parse(stdin.readLineSync()!);
+    print(x / y);
+  } on FormatException catch (e) {
+    print("formate exception on error $e");
   } catch (e) {
-    print("$e invalid input !");
+    print("invalid input");
+  } finally {
+    print("thank you");
   }
+
+  // List l1 = [1, 2, 3, 4, 5];
+
+  // try {
+  //   print(l1[6]);
+  // } on RangeError catch (e) {
+  //   print("Enter sahi input ! $e");
+  // } catch (e) {
+  //   print("$e invalid input !");
+  // }
 }
