b05380463bab80e4e8703070512cf02aeac57d41
akhil <akhilsodvadiya7@gmail.com>
2024-01-31 11:00:12 +0530

    31-01-2024

--- Dart OOPS/factory/main.dart
+++ Dart OOPS/factory/main.dart
@@ -1,19 +1,20 @@
 import 'dart:io';
 
-
 // 1
-// 2 9 10
-// 3 8 11 14
+// 2 9 
+// 3 8 10 
+// 4 7 11 14
 // 5 6 12 13 15
 
 void main() {
   int x = 1;
+  int p = 5;
 
   for (int i = 1; i <= 5; i++) {
     for (int j = 1; j <= i; j++) {
-      stdout.write("$x ");
+      
     }
-    x = i;
+    x++;
     print("");
   }
 }
--- temp/exam.c
+++ temp/exam.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-void printPattern() {
-    int rows = 5; // Number of rows in the pattern
 
-    // Loop through each row and column to print the pattern
-    for (int i = 0; i < rows; i++) {
-        int currentNumber = i + 1;
-
-        for (int j = 0; j <= i; j++) {
-            printf("%d ", currentNumber);
-
-            // Update currentNumber based on the logic
-            if (j % 2 == 0) {
-                currentNumber += (rows - j);
-            } else {
-                currentNumber += (rows + j);
-            }
-        }
-
-        printf("\n");
-    }
-}
+// 1
+// 2 9 
+// 3 8 10 
+// 4 7 11 14
+// 5 6 12 13 15
 
 int main() {
-    printPattern();
+    
+int x = 1;
+  int p = 5;
+
+  for (int i = 1; i <= 5; i++) {
+
+    for (int j = 1; j <= i; j++) {
+      
+
+		printf("%d ",x);
+
+    }
+	
+    printf("\n");
+  }
+
     return 0;
 }
