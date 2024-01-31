6c55ea43262bb0d880a42be614c4e773e2c84561
akhil <akhilsodvadiya7@gmail.com>
2024-01-31 13:50:28 +0530

    31-01-2024

--- temp/exam.c
+++ temp/exam.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
-
-
-// 1
+// 1 
 // 2 9 
 // 3 8 10 
 // 4 7 11 14
 // 5 6 12 13 15
 
-int main() {
-    
-int x = 1;
-  int p = 5;
 
-  for (int i = 1; i <= 5; i++) {
+int main()
+{
+	int x = 1;
+	int p = 9;
+	int q = -2;
 
-    for (int j = 1; j <= i; j++) {
-      
-
-		printf("%d ",x);
-
-    }
-	
-    printf("\n");
-  }
-
+    for(int i=1; i<=5; i++)
+	{
+		x = i;
+		for(int j=1; j<=i; j++)
+		{
+			if(j==1)
+			{
+				printf("%d ",x);
+				for(int a = 1; a<x; a++)
+				{
+					p-=2;
+					q+=2;
+				}
+				
+			}
+			else if(j==5)
+			{
+				x+=2;
+				printf("%d ",x);
+			}
+			else if(j%2==0)
+			{
+				x = x + p;
+				printf("%d ",x);
+			}
+			else
+			{
+				x = x + q;
+				printf("%d ",x);
+			}
+		}
+		p = 9;
+		q = -2;
+		printf("\n");
+	}
     return 0;
 }
