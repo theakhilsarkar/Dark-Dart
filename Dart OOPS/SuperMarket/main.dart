d421788d8b6ba48896e528a5e81ba32529144e40
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2024-03-01 20:41:57 +0530

    01-03-2024

--- Dart OOPS/SuperMarket/main.dart
+++ Dart OOPS/SuperMarket/main.dart
@@ -1,29 +1,29 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'customer.dart';
-import 'list.dart';
-import 'product.dart';
+// import 'customer.dart';
+// import 'list.dart';
+// import 'product.dart';
 
-void main() {
-  Customer c1 = Customer();
+// void main() {
+//   Customer c1 = Customer();
 
-  List<Product> l1 = List.generate(
-      productList.length, (index) => Product.fromMap(productList[index]));
+//   List<Product> l1 = List.generate(
+//       productList.length, (index) => Product.fromMap(productList[index]));
 
-  for (int i = 0; i < l1.length; i++) {
-    print('${l1[i].pro_id}');
-    print('${l1[i].pro_name} ${l1[i].pro_disc}% ${l1[i].pro_price}\n');
-  }
+//   for (int i = 0; i < l1.length; i++) {
+//     print('${l1[i].pro_id}');
+//     print('${l1[i].pro_name} ${l1[i].pro_disc}% ${l1[i].pro_price}\n');
+//   }
 
-  c1.addCutomer();
-  c1.selectProduct();
+//   c1.addCutomer();
+//   c1.selectProduct();
 
-  print('Your Bill -> ')
-  for (int i = 0; i < cartList.length; i++) {
-    if (cartList[i] == l1[i].pro_id) {
-      double finalPrice = (l1[i].pro_price*l1[i].pro_disc!)/100 + l1[i].pro_price!;
-      print('${l1[i].pro_id}');
-      print('${l1[i].pro_name} ${l1[i].pro_disc}% ${l1[i].pro_price} ${l1[i].pro_price} $finalPrice\n');
-    }
-  }
-}
+//   print('Your Bill -> ');
+//   for (int i = 0; i < cartList.length; i++) {
+//     if (cartList[i] == l1[i].pro_id) {
+//       // double finalPrice = (l1[i].pro_price*l1[i].pro_disc!)/100 + l1[i].pro_price!;
+//       print('${l1[i].pro_id}');
+//       print('${l1[i].pro_name} ${l1[i].pro_disc}% ${l1[i].pro_price} ${l1[i].pro_price} $finalPrice\n');
+//     }
+//   }
+// }
