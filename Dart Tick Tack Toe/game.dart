e215530dd1609de0575011a4975f799b7c394023
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2024-03-03 09:31:05 +0530

    03-03-2024

--- Dart Tick Tack Toe/game.dart
+++ Dart Tick Tack Toe/game.dart
@@ -4,6 +4,8 @@ class GameEngine {
   late int x, y;
   bool isGameWin = false;
   bool crossTurn = true;
+  bool isAiTurn = true;
+  bool isUserIsAi = true;
   int gameEndCount = 9;
   int gameTurnCount = 1;
   String winnerStatus = 'Draw';
@@ -12,6 +14,17 @@ class GameEngine {
     [3, 4, 5],
     [6, 7, 8],
   ];
+
+  void askForUserOrAi() {
+    print("User or Ai : ");
+    stdout.write("Enter 1 for User & 2 for AI : ");
+    int x = int.parse(stdin.readLineSync()!);
+    if (x == 1) {
+      isUserIsAi = true;
+    } else {
+      isUserIsAi = false;
+    }
+  }
 
   bool checkWinner() {
     // row - 1
@@ -118,7 +131,6 @@ class GameEngine {
   }
 
   void zeroInput() {
-    showGameBoard();
     print("\n");
     print('O - Turn -:>');
     print('Enter position - (x,y) : ');
@@ -137,7 +149,6 @@ class GameEngine {
   }
 
   void crossInput() {
-    showGameBoard();
     print("\n");
     print('X - Turn -:>');
     print('Enter position - (x,y) : ');
@@ -155,19 +166,82 @@ class GameEngine {
     print("\n");
   }
 
+  // * AI Method
+
+  void aiInputGenerator() {
+    switch (gameTurnCount) {
+      case 1:
+        gameBoard[0][2] = 'X';
+        showGameBoard();
+        break;
+      case 3:
+        if (gameBoard[2][0] != 'O') {
+          gameBoard[2][0] = 'X';
+          showGameBoard();
+        } else if (gameBoard[0][0] != '0') {
+          gameBoard[0][0] = 'X';
+          showGameBoard();
+        } else if (gameBoard[2][2] != '0') {
+          gameBoard[0][0] = 'X';
+          showGameBoard();
+        }
+        break;
+      case 5:
+        if (gameBoard[0][2] == gameBoard[2][0] &&
+            gameBoard[2][0] == 'X' &&
+            gameBoard[1][1] != 'O') {
+          gameBoard[1][1] = 'X';
+          showGameBoard();
+        } else if (gameBoard[0][0] == gameBoard[0][2] &&
+            gameBoard[0][2] == 'X' &&
+            gameBoard[0][1] != 'O') {
+          gameBoard[0][1] = 'X';
+          showGameBoard();
+        } else if (gameBoard[2][2] == 'X') {
+          gameBoard[1][2] = 'X';
+          showGameBoard();
+        } else if (gameBoard[1][1] == 'O' && gameBoard[2][2] != 'O') {
+          gameBoard[2][2] = 'X';
+          showGameBoard();
+        }
+
+        break;
+    }
+  }
+
   // ! main method for start game
 
   void startGame() {
-    if (crossTurn) {
-      crossInput();
-      isGameWin = checkWinner();
-      crossTurn = false;
-      gameTurnCount++;
+    if (isUserIsAi) {
+      if (crossTurn) {
+        showGameBoard();
+        crossInput();
+        isGameWin = checkWinner();
+        crossTurn = false;
+        gameTurnCount++;
+      } else {
+        showGameBoard();
+        zeroInput();
+        isGameWin = checkWinner();
+        crossTurn = true;
+        gameTurnCount++;
+      }
     } else {
-      zeroInput();
-      isGameWin = checkWinner();
-      crossTurn = true;
-      gameTurnCount++;
+      print("ai campuse --->");
+      if (isAiTurn) {
+        print("ai turn ---> turn count - $gameTurnCount");
+        aiInputGenerator();
+        isGameWin = checkWinner();
+        isAiTurn = false;
+        gameTurnCount++;
+      } else {
+        print("zero turn ---> turn count - $gameTurnCount");
+        // showGameBoard();
+        zeroInput();
+        isGameWin = checkWinner();
+        isAiTurn = true;
+        gameTurnCount++;
+      }
     }
 
     if (isGameWin || gameEndCount == 0) {
--- Dart Tick Tack Toe/main.dart
+++ Dart Tick Tack Toe/main.dart
@@ -3,5 +3,6 @@ import 'game.dart';
 void main() {
   GameEngine gameEngine = GameEngine();
   gameEngine.showGameBoardIndex();
+  gameEngine.askForUserOrAi();
   gameEngine.startGame();
 }
