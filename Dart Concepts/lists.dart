d93a0b255e5954fd4fcb14d6b5bd777b1a77302b
akhil <akhilsodvadiya7@gmail.com>
2024-03-07 11:09:12 +0530

    07-03-2024

---
+++ Dart Concepts/lists.dart
@@ -0,0 +1,37 @@
+void main() {
+  // declaration
+  List growableList = [1, 2, 3, "hello", true];
+  List reversed = growableList.reversed.toList();
+  Iterable i1 = [1, 2, "itr"];
+
+  List list = List.filled(5, 0, growable: true);
+  List list2 = List.generate(5, (index) => index + 1, growable: true);
+  List list3 = const [1, 2, 3];
+  List<int> list4 = [1, 2, 3];
+
+  list.add(10);
+  list2.add(6);
+  list3.add(11);
+
+  print(list3);
+  print(list2);
+  print(list);
+
+  // growableList.forEach((element) {
+  //   print(element);
+  // });
+
+  // Iterable l1 = growableList
+  //     .map(
+  //       (e) => true,
+  //     )
+  //     .toList();
+  // print(l1);
+  // print("");
+  // print(i1.any((element) => element == "hello"));
+  // print(growableList.any((element) => element == "hello"));
+  // print(i1.isEmpty);
+  // print(i1.first);
+  // print(i1.indexed);
+  // print(reversed);
+}
