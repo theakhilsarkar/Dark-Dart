a46c8d4f1906aae132d9bfb9c9d56b37ae94df83
RW <prafulpatnecha@gmail.com>
2024-07-01 11:22:34 +0530

    01-07-2024

--- Exception/main.dart
+++ Exception/main.dart
@@ -1,19 +1,30 @@
 import 'dart:io';
 
 void main() {
-  int x = 10;
-  int y = 0;
-  String name = 'name';
+  // exception handling : error
+  // runtime error
+  // try..catch
 
-  try {
-    y = int.parse(name);
-  }
-  // on FormatException catch (e) {
-  //   print(e);
-  //   }
-  catch (e) {
-    print(e.toString() + "error occured");
+  // print(10 / 0); // divide by zero exception
+  int? age;
+  stdout.write("Enter your age : ");
+
+  // Format Exception : when you convert any data which is non-convertable.
+  // 22 -> int -> 22
+  // name -> int -> format exception
+
+  try // its try/check error comes or not
+  {
+    age = int.parse(stdin.readLineSync()!);
+  } on FormatException {
+    print("format exception caught");
+  } catch (error) {
+    print("Value must be integer / invalid value !");
   } finally {
-    print("jp bhi ");
+    print(age ?? 0);
   }
+
+  // print(age ?? 0); // null - replacement operator : ??
+  
 }
+
