5307210126129c1194b8b9e1db62d0b3db0b27b2
RW <prafulpatnecha@gmail.com>
2024-07-02 11:51:42 +0530

    02-07-2024

--- LeetCode/find_target_index.dart
+++ LeetCode/find_target_index.dart
@@ -1,15 +1,19 @@
 class Solution {
-  int searchInsert(List<int> nums, int target) {
+  List<int> twoSum(List<int> nums, int target) {
+    List<int> l1 = [];
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] == target) {
-        return i;
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          l1.add(i);
+          l1.add(j);
+        }
       }
     }
-   
+    return l1;
   }
 }
 
 void main() {
   Solution s1 = Solution();
-  print(s1.searchInsert([1, 3, 5, 6], 5));
+  print(s1.twoSum([3, 2, 4], 8));
 }
