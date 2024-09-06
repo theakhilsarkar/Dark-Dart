1e351faf4f295f04f063bb96f34a5079ea9382c4
akhil <akhilsodvadiya7@gmail.com>
2024-09-06 20:28:10 +0530

    ....

--- LeetCode/3sum.dart
+++ LeetCode/3sum.dart
@@ -21,6 +21,11 @@ class Solution {
       }
     }
 
+    for (int i = 0; i < ans.length; i++) {
+      for (int j = 0; j < 3; j++) {
+        var r1 = ans[i][j] - ans[i + 1][j] == 0;
+      }
+    }
     return ans;
   }
 }
