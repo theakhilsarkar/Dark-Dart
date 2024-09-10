c47349633440b07ef56822da58a34ea2dcaffda7
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2024-09-11 04:58:13 +0530

    ....

--- LeetCode/3sum.dart
+++ LeetCode/3sum.dart
@@ -2,7 +2,7 @@ import 'dart:collection';
 
 void main() {
   Solution s1 = Solution();
-  List<int> num = [-1, 0, 1, 2, -1, -4];
+  List<int> num = [0, 0, 0, 0];
   print(s1.threeSum(num));
 }
 
@@ -21,6 +21,19 @@ class Solution {
       }
     }
 
+    for (int i = 0; i < ans.length; i++) {
+      for (int j = i + 1; j < ans.length; j++) {
+        int duplicate = 0;
+        for (int k = 0; k < 3; k++) {
+          if (ans[i][k] == ans[j][k]) {
+            duplicate++;
+          }
+        }
+        if (duplicate == 3) {
+          ans.removeAt(j);
+        }
+      }
+    }
     return ans;
   }
 }
