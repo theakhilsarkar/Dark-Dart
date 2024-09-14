559509669c4bcc7ef435aa3f4b40cb27afecd62d
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-09-14 19:27:03 +0530

    ....

--- LeetCode/4sum_target.dart
+++ LeetCode/4sum_target.dart
@@ -1,27 +1,13 @@
 void main() {
   Solution s1 = Solution();
-  List<int> num = [2, 2, 2, 2, 2];
-  print(s1.fourSum(num, 8));
+  List<int> num = [1, 0, -1, 0, -2, 2];
+  print(s1.fourSum(num, 0));
 }
 
 class Solution {
   List<List<int>> fourSum(List<int> nums, int target) {
     List<List<int>> ans = [];
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        for (int k = j + 1; k < nums.length; k++) {
-          for (int l = k + 1; l < nums.length; l++) {
-            if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
-              List<int> res = [nums[i], nums[j], nums[k], nums[l]];
-              res.sort();
-              if (!ans.contains(res)) {
-                ans.add(res);
-              }
-            }
-          }
-        }
-      }
-    }
+    
     return ans;
   }
 }
