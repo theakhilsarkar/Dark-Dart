b05d7475d9049704843a4c83871af367d747d723
RW <prafulpatnecha@gmail.com>
2024-10-04 11:03:25 +0530

    ..

--- LeetCode/combination_sum_39.dart
+++ LeetCode/combination_sum_39.dart
@@ -6,21 +6,18 @@ class Solution {
       if (l1[i] == target) {
         ans.add([l1[i]]);
       }
-       
-        if(target%l1[i]==0 && l1[i]!=target)
-        {
-          int x = l1[i];
-          List<int> y = [];
-          while(x<=target){
-            y.add(l1[i]);
-            x+=l1[i];
-          }
-          if(y.isNotEmpty)
-          {
-            ans.add(y);
-          }
-        }
 
+      if (target % l1[i] == 0 && l1[i] != target) {
+        int x = l1[i];
+        List<int> y = [];
+        while (x <= target) {
+          y.add(l1[i]);
+          x += l1[i];
+        }
+        if (y.isNotEmpty) {
+          ans.add(y);
+        }
+      }
 
       for (int j = i + 1; j < l1.length; j++) {
         if (l1[i] + l1[j] == target) {
@@ -39,7 +36,7 @@ class Solution {
 
 void main() {
   Solution s1 = Solution();
-  print(s1.combinationSum([2,3,7],18));
+  print(s1.combinationSum([2, 3, 7], 18));
 }
 
 // combination of sum of 7
