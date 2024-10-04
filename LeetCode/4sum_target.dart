eba61f3480c730f9501f3acdc741f6ec34500e9f
RW <prafulpatnecha@gmail.com>
2024-10-04 10:37:21 +0530

    ......

--- LeetCode/4sum_target.dart
+++ LeetCode/4sum_target.dart
@@ -7,21 +7,7 @@ void main() {
 class Solution {
   List<List<int>> fourSum(List<int> nums, int target) {
     List<List<int>> ans = [];
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        for (int k = j + 1; k < nums.length; k++) {
-          for (int l = k + 1; l < nums.length; l++) {
-            if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
-              List<int> res = [nums[i], nums[j], nums[k], nums[l]];
-              res.sort();
-              if (!ans.contains(res)) {
-                ans.add(res);
-              }
-            }
-          }
-        }
-      }
-    }
+      
     return ans;
   }
 }
