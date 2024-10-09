b023a8aaf3afab6421b878e8f7fe6f9369b2c271
akhil <akhilsodvadiya7@gmail.com>
2024-10-09 18:39:51 +0530

    ..........

---
+++ LeetCode/permutation_46.dart
@@ -0,0 +1,13 @@
+class Solution {
+  List<List<int>> permute(List<int> nums) {
+    List<List<int>> ans = [];
+    
+    
+    return ans;
+  }
+}
+
+void main() {
+  Solution s1 = Solution();
+  s1.permute([1, 2, 3]);
+}
