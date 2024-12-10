dafdfe948c6f1c8a5839a1b7ce101462d3ec0a62
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-12-10 12:48:11 +0530

    ...

--- exam/dart_random.dart
+++ exam/dart_random.dart
@@ -1,29 +1,30 @@
 import 'dart:math';
 
 import 'adv_flutter_exam1.dart';
-import 'c_questions.dart';
-import 'flutter_3pm.dart';
+
 import 'flutter_11am.dart';
+import 'flutter_3pm.dart';
+import 'php_questions.dart';
 
 void main() {
   Random random = Random();
-  int name = random.nextInt(2);
+  int name = random.nextInt(13);
 
-  int question1 = random.nextInt(30);
-  int question2 = random.nextInt(30);
-  int question3 = random.nextInt(30);
-  int question4 = random.nextInt(30);
-  int question5 = random.nextInt(30);
+  int question1 = random.nextInt(24);
+  int question2 = random.nextInt(24);
+  int question3 = random.nextInt(24);
+  int question4 = random.nextInt(24);
+  int question5 = random.nextInt(24);
 
   print("\n");
-  print("Name : ${studentListOf11Am[0]}");
+  print("Name : ${studentListOf3pm[name]}");
   print("");
 
-  print("Q1 : ${advanceFlutterExam1[question1]}");
-  print("Q2 : ${advanceFlutterExam1[question2]}");
-  print("Q3 : ${advanceFlutterExam1[question3]}");
-  print("Q4 : ${advanceFlutterExam1[question4]}");
-  print("Q5 : ${advanceFlutterExam1[question5]}");
+  print("Q1 : ${php_questions_list[question1]}");
+  print("Q2 : ${php_questions_list[question2]}");
+  print("Q3 : ${php_questions_list[question3]}");
+  print("Q4 : ${php_questions_list[question4]}");
+  print("Q5 : ${php_questions_list[question5]}");
 
   print("\n");
 }
